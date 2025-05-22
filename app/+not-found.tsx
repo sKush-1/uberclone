d608c41394b0a06c4cbdfe37a1d60e138dc078be
sumitkush-srchout <sumit@srchoutsoftware.com>
2025-05-22 13:52:12 +0530

    setup project with native wind

---
+++ app/+not-found.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <View>
+        <Text>Not found</Text>
+      </View>
+    
+    </>
+  );
+}
+
+
---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+
+import '../global.css';
+
+export default function RootLayout() {
+  
+
+  
+
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{headerShown: false}} />
+      <Stack.Screen name='not-found' options={{headerShown: false}} />
+      </Stack>
+  );
+}
